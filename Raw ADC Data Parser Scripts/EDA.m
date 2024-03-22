4794e92aaf2058de91635a69061c1a777f5da470
BartlomiejWos <bartlomiej.wos01@gmail.com>
2024-03-22 11:03:38 +0100

    raw data parser

---
+++ Raw ADC Data Parser Scripts/EDA.m
@@ -0,0 +1,13 @@
+
+% Parameters based on mmWave sensor configuration 
+
+filename = "capture_test.bin";  % The name of the file containing raw data
+numADCSamples = 256;    % number of ADC samples per chirp 
+numADCBits = 16;        % number of ADC bits per sample 
+numRX = 4;              % number of receivers 
+isReal = false;         % set to "true" if real only data, "false" if complex data
+
+
+
+retval = rawDataParser(filename, numADCSamples, numADCBits, isReal);
+disp(retval);
--- Raw ADC Data Parser Scripts/rawDataParser.m
+++ Raw ADC Data Parser Scripts/rawDataParser.m
@@ -1,17 +1,18 @@
 
 
-%%% This script is used to read the binary file produced by the DCA1000 and Mmwave Studio
+%% This script is used to read the binary file produced by the DCA1000 and Mmwave Studio
 %
 % Description:
 %   This function parses raw data ...
+%
 % Syntax:
 %   retVal = rawDataParser(fileName, numADCSamples, numADCBits, numRX, isReal)
 %
 % Input:
-%   fileName - The name of the file containing raw data.
-%   numADCSamples - number of ADC samples per chirp (256)
-%   numADCBits - number of ADC bits per sample (16)
-%   numRX - number of receivers (4)
+%   fileName - The name of the file containing raw data
+%   numADCSamples - number of ADC samples per chirp 
+%   numADCBits - number of ADC bits per sample 
+%   numRX - number of receivers 
 %   isReal - set to "true" if real only data, "false" if complex data
 %
 % Output:
@@ -21,16 +22,14 @@
 %   retVal = rawDataParser('datafile.bin', 256, 16, 4, true);
 %
 
+
 function [retVal] = rawDataParser(fileName, ...
                                   numADCSamples, ...
                                   numADCBits, ...
                                   numRX, ...
                                   isReal)
 
-    %numLanes = 2;          % do not change. number of lanes is always 2
- 
-
-    fid = fopen(fileName,'r');          % open file
+    fid = fopen(fileName,'r');          % open file for binary read acces 
     adcData = fread(fid, 'int16');      % read .bin file
 
 
@@ -59,15 +58,15 @@ function [retVal] = rawDataParser(fileName, ...
         counter = 1;
         for i=1:4:fileSize-1
             LVDS(1,counter) = adcData(i) + sqrt(-1) * adcData(i+2); 
-            LVDS(1,counter+1) = adcData(i+1)+ sqrt(-1) * adcData(i+3); 
+            LVDS(1,counter+1) = adcData(i+1) + sqrt(-1) * adcData(i+3); 
             counter = counter + 2;
         end
       
         LVDS = reshape(LVDS, numADCSamples*numRX, numChirps);   % create column for each chirp
-        LVDS = LVDS.'; %each row is data from one chirp
+        LVDS = LVDS.'; % each row is data from one chirp
     end
 
-    %organize data per RX
+    % organize data per RX
     adcData = zeros(numRX,numChirps*numADCSamples);
     for row = 1:numRX
         for i = 1: numChirps
@@ -76,4 +75,5 @@ function [retVal] = rawDataParser(fileName, ...
     end
     
     retVal = adcData; % return receiver data
+    % retVal = fid;
 end
